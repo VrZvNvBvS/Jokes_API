a0ed93b81f5a404b6fe56582ec5b990316cb06e8
Jerry <3u.ngn@pm.me>
2022-09-17 18:10:25 -0700

    Update mongoose.config.js

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const DATABASE_NAME = "jokesDB"
 
 
-mongoose.connect("mongodb+srv://Random:1234567890@supervirgo.m337wu3.mongodb.net/jokesDB?retryWrites=true&w=majority" + DATABASE_NAME, {
+mongoose.connect(MONGO_url + DATABASE_NAME, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 })
